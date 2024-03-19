92e5ddf4825f74c1b4888d62b123b4656d792bc0
Adesh Pathak <adesh.pathak@MacBook-Pro.local>
2024-03-19 13:06:24 +0530

    adding doamin entities in domain core

---
+++ order-service/order-domain/order-domain-core/src/main/java/com/food/ordering/system/order/service/domain/entity/Customer.java
@@ -0,0 +1,8 @@
+package com.food.ordering.system.order.service.domain.entity;
+
+import java.util.UUID;
+
+import com.food.ordering.system.domain.entity.BaseEntity;
+
+public class Customer extends BaseEntity<UUID> {
+}
